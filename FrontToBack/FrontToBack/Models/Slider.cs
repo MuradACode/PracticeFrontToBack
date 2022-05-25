65fc07ffe6a95c7aeb697f980e2a882b5b595004
Murad Alizada <tu7nxdvhu@aztu.local>
2022-05-25 13:42:13 +0400

    Practice Front to Back task

---
+++ FrontToBack/FrontToBack/Models/Slider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Models
+{
+    public class Slider : BaseEntity
+    {
+        public string ImageUrl { get; set; }
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+        public string Desc { get; set; }
+        public string RedirectedUrl { get; set; }
+        public string RedirectedUrlText { get; set; }
+        public int Price { get; set; }
+    }
+}
